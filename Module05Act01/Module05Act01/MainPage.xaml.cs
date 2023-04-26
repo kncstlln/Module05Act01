10d79a85c39257aedac5765314330307a65fcaff
Kane Castillano <castillano.kaneerryl@auf.edu.ph>
2023-04-26 18:17:36 +0800

    first commit

---
+++ Module05Act01/Module05Act01/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Module05Act01
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            mySwitch.Toggled += MySwitch_Toggled;
+        }
+
+        private void MySwitch_Toggled(object sender, ToggledEventArgs e)
+        {
+            // Handle the Toggled event here
+            if (e.Value)
+            {
+                myLabel.Text = "Switch is ON";
+            }
+            else
+            {
+                myLabel.Text = "Switch is OFF";
+            }
+        }
+    }
+}
